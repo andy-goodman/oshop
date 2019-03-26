9171ac98d723fb0630c51d04111fd9a4538f2e60
dobryakoff <andrey@dobryakov.biz>
2019-03-26 23:48:07 +0300

    firebase project init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBcjgCWmYrHr7fGiUeRt3VRpaV-UA7QeVs",
+    authDomain: "andyshop1024.firebaseapp.com",
+    databaseURL: "https://andyshop1024.firebaseio.com",
+    projectId: "andyshop1024",
+    storageBucket: "andyshop1024.appspot.com",
+    messagingSenderId: "932811471190"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBcjgCWmYrHr7fGiUeRt3VRpaV-UA7QeVs",
+    authDomain: "andyshop1024.firebaseapp.com",
+    databaseURL: "https://andyshop1024.firebaseio.com",
+    projectId: "andyshop1024",
+    storageBucket: "andyshop1024.appspot.com",
+    messagingSenderId: "932811471190"
+  }
 };
 
 /*
